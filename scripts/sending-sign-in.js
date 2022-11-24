5e7a3ce451a2ec4501176b971822c67415de3943
lev tarasov <56757711+levil664@users.noreply.github.com>
2022-11-24 15:33:26 +0500

    nastya's changes

---
+++ scripts/sending-sign-in.js
@@ -0,0 +1,11 @@
+const url = 'https://my-json-server.typicode.com/typicode/demo/profile'
+
+// fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Authorization': 'Basic base64codedloginadpassword==',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//     "mode":"cors"
+//   });
