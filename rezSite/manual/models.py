b4e348b9e2c3c3dab674d8e3d781d8485fcbba2c
Anastasia Demina <fioB7@yandex.ru>
2022-12-16 20:25:27 +0500

    rename dir

--- rezSite/manual/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Manual(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="static/pictures/")
-
--- rezSite/manual/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path(r'', manual),
-    path(r'', to_map)
-]
