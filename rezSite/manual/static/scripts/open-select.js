91bd08b03b5dca17581a791b6db2ba599dadde61
Anastasia Demina <fioB7@yandex.ru>
2022-12-18 22:32:31 +0500

    small touches for beauty

--- rezSite/manual/static/scripts/open-select.js
+++ rezSite/manual/static/scripts/open-select.js
@@ -4,10 +4,12 @@ const navigationOptions = document.querySelectorAll('.navigation_list li');
 
 navigationHeader.addEventListener('click', () => {
     navigationList.classList.toggle('hidden');
+    navigationHeader.style.borderBottom = '1px solid #63ADD0';
 })
 
 navigationOptions.forEach((option) => {
     option.addEventListener('click', function () {
+        navigationHeader.style.borderBottom = 'none';
         const value = this.value;
         window.scrollTo(0, Number(value))
         navigationList.classList.add('hidden');
