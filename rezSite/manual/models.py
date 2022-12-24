a21044078d273cffa08e0f2a081c7258c797c19b
GreatSherhebe1 <sherhebe1@mail.ru>
2022-12-24 21:16:23 +0500

    rework models & makemigrations

--- rezSite/manual/models.py
+++ rezSite/manual/models.py
@@ -5,4 +5,3 @@ class Manual(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     photo = models.ImageField(upload_to="static/pictures/")
-
--- rezSite/map/models.py
+++ rezSite/map/models.py
@@ -5,8 +5,18 @@ class MapRoute(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 
 class Point(models.Model):
     x = models.FloatField()
     y = models.FloatField()
     routeId = models.ForeignKey(MapRoute, on_delete=models.CASCADE)
+    is_street_cross = models.BooleanField()
+    name = models.CharField(max_length=50, null=True, default=None, blank=True)
+    description = models.CharField(max_length=250, null=True, default=None, blank=True)
+    picture = models.ImageField(upload_to="static/pictures/", null=True, default=None, blank=True)
+
+    def __str__(self):
+        return self.name
