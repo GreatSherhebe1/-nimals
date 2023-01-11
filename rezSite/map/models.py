04e6e76989d70689ad257af4a1f35bae466a4ddb
GreatSherhebe1 <sherhebe1@mail.ru>
2023-01-11 23:44:52 +0500

    add description + points (name, desc, images)

--- rezSite/map/models.py
+++ rezSite/map/models.py
@@ -16,6 +16,7 @@ class Point(models.Model):
     is_street_cross = models.BooleanField()
     name = models.CharField(max_length=50, null=True, default=None, blank=True)
     description = models.CharField(max_length=250, null=True, default=None, blank=True)
+    Описание = models.TextField(null=True, default=None, blank=True)
     picture = models.ImageField(upload_to="static/pictures/", null=True, default=None, blank=True)
 
     def __str__(self):
